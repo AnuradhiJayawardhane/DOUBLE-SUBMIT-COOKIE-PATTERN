b5b4f9bd81ac7a22a76649b6275bf3cfa6df7a4c
AnuradhiJayawardhane <55041251+AnuradhiJayawardhane@users.noreply.github.com>
2019-10-11 13:30:33 +0530

    Update indextest.php

--- indextest.php
+++ indextest.php
@@ -7,10 +7,11 @@ if ( ! empty( $_POST ) ) {
       $uname = $_POST['username'];
       $pwd = $_POST['password'];
       if($uname == 'admin' && $pwd == 'admin'){
-       $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
-       $session_id = session_id();
-
-       //create cookies
+	      
+       //create session id and csrf cookies
+        $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+        $session_id = session_id();
+       
         setcookie('sessionCookie',$session_id,time()+ 60*60*24*365 ,'/');
         setcookie('csrfTokenCookie',$_SESSION['token'],time()+ 60*60*24*365 ,'/');
 
